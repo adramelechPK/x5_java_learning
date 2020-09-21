6afa901d36ce2285ea4a169b62775d9b4cfb56e5
adramelechPK <prd87@mail.ru>
2020-09-21 23:39:04 +0400

    hometask 4 задание 1

--- lesson4/src/Main.java
+++ lesson4/src/Main.java
@@ -1,11 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        homeTask1();
+
+
+    }
+
+    public static void homeTask1() {
         Shape[] shape = new Shape[3];
         shape[0] = new Triangle();
         shape[1] = new Circle();
         shape[2] = new Rectangle();
-        for (Shape sh: shape){
-            System.out.println("Площадь фигуры: "+sh.square());
+        for (Shape sh : shape) {
+            System.out.println("Площадь фигуры: " + sh.square());
         }
     }
 }
